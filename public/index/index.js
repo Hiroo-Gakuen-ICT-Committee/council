e07b0535180cbc39864ef620d4a8357ff07f0cbc
Tomoro0726 <tomoro0726@gmail.com>
2021-11-21 16:20:27 +0900

    アニメーションつけた

---
+++ public/index/index.js
@@ -0,0 +1,20 @@
+var object_8 = document.querySelector('.header_objects_8');
+anime({
+  targets: object_8,
+  translateX: 270,
+  translateY: 270
+});
+var object_17 = document.querySelector('.header_objects_17');
+anime({
+  targets: object_17,
+  translateX: -270,
+  translateY: -270,
+
+});
+var object_7 = document.querySelector('.header_objects_7');
+anime({
+  targets: object_7,
+  translateX: 270,
+  translateY: -270,
+
+});
