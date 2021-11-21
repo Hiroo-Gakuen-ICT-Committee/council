f5c4ed8eedb0455932ebbd6df48c07f1412e4f48
Tomoro0726 <tomoro0726@gmail.com>
2021-11-21 16:51:08 +0900

    どうぞ

--- public/index/index.js
+++ public/index/index.js
@@ -2,36 +2,72 @@ var object_8 = document.querySelector('.header_objects_8');
 anime({
   targets: object_8,
   translateX: 270,
-  translateY: 270
+  translateY: 270,
+  delay: 400
 });
 var object_17 = document.querySelector('.header_objects_17');
 anime({
   targets: object_17,
   translateX: -270,
   translateY: -270,
+  delay: 400
 
 });
 var object_7 = document.querySelector('.header_objects_7');
 anime({
   targets: object_7,
-  translateX: 270,
-  translateY: -270,
-  rotateZ: -90
+  translateX: 1540,
+  translateY: -1540,
+  rotateZ: -90,
+  delay: 400
 });
 var object_6 = document.querySelector('.header_objects_6');
 anime({
   targets: object_6,
   translateX: 270,
   translateY: -270,
+  delay: 400
 });
 var object_3 = document.querySelector('.header_objects_3');
 anime({
   targets: object_3,
   translateX: 270,
   translateY: 270,
+  delay: 400
 });
 var object_1 = document.querySelector('.header_objects_1');
 anime({
   targets: object_1,
-  rotateZ: 360
+  rotateZ: 360,
+  delay: 400
+});
+var object_4 = document.querySelector('.header_objects_4');
+anime({
+  targets: object_4,
+  rotateZ: -360,
+  delay: 400
+});
+var object_16 = document.querySelector('.header_objects_16');
+anime({
+  targets: object_16,
+  rotateZ: -180,
+  delay: 400
+});
+var object_9 = document.querySelector('.header_objects_9');
+anime({
+  targets: object_9,
+  translateX: 540,
+  delay: 400
+});
+var object_14 = document.querySelector('.header_objects_14');
+anime({
+  targets: object_14,
+  translateX: -1600,
+  delay: 400
+});
+var object_5 = document.querySelector('.header_objects_5');
+anime({
+  targets: object_5,
+  translateY: +270,
+  delay: 400
 });
